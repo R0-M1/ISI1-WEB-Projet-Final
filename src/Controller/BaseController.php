9edfa65da43c6dd6c2f14109dd2f466598b805cf
Romain <romain.aillaud@free.fr>
2025-01-07 23:56:41 +0100

    dernier commit

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -22,6 +22,12 @@ class BaseController
 
         $loader = new \Twig\Loader\FilesystemLoader('../src/View');
         $twig = new \Twig\Environment($loader);
+
+        $actual_link = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]";
+        $base_uri = rtrim(dirname($_SERVER['REQUEST_URI']), '/') . '/';
+        $actual_link .= $base_uri;
+        $twig->addGlobal('base_url', $actual_link);
+
         return $twig->render($view, $data);
     }
 }
